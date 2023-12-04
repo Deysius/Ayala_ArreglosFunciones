fb8d3c07d74854346ce724eb233013fbeac2a33e
Josue Ayala <josue.ayala.flores@udla.edu.ec>
2023-12-03 22:15:59 -0500

    Agrego la operacion de suma de matrices a mi funcion

--- Suma de matrices con funciones.c
+++ Suma de matrices con funciones.c
@@ -3,5 +3,18 @@
 #include<time.h>
 
 void sumaMatriz(int filas, int columnas,int matriz[filas][columnas],int matriz2[filas][columnas],int sumatriz[filas][columnas]);
+int main(){
+return 0;
+}
+void sumaMatriz(int filas, int columnas,int matriz[filas][columnas],int matriz2[filas][columnas],int sumatriz[filas][columnas]){
+for (int i = 0; i < filas; i++)
+    {
+          for (int j = 0; j < columnas; j++) {
+            sumatriz[i][j]=matriz[i][j]+matriz2[i][j];
+            printf("%d\t", sumatriz[i][j]);
+        }
+        printf ("\n");
+    }
+}
 
 
