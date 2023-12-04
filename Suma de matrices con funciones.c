e67fab230b30ebc332752a41e053a85e7fc93a50
Josue Ayala <josue.ayala.flores@udla.edu.ec>
2023-12-03 22:14:21 -0500

    Agrego mis librerias y agrego mi funcion

---
+++ Suma de matrices con funciones.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+void sumaMatriz(int filas, int columnas,int matriz[filas][columnas],int matriz2[filas][columnas],int sumatriz[filas][columnas]);
+
+
