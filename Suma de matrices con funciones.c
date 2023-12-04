41669b311184c3e7fdce4a1e88a04b43298e0c44
Josue Ayala <josue.ayala.flores@udla.edu.ec>
2023-12-03 22:22:47 -0500

    Agrego comentarios

--- Suma de matrices con funciones.c
+++ Suma de matrices con funciones.c
@@ -1,8 +1,9 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<time.h>
-
+//Con estas librerias puedo pedir numeros aleatorios
 void sumaMatriz(int filas, int columnas,int matriz[filas][columnas],int matriz2[filas][columnas],int sumatriz[filas][columnas]);
+//Utilizo un void, ya que no me arroja un resultado
 int main(){
 int filas, columnas;
 srand (time(NULL));
@@ -10,6 +11,7 @@ srand (time(NULL));
     scanf("%d", &filas);
     printf("Ingrese el número de columnas de la matriz: ");
     scanf("%d", &columnas);
+    //Pido al usuario que establezca la dimension de la matriz
     printf("Primera Matriz:\n");
     int matriz[filas][columnas];
     int matriz2[filas][columnas];
@@ -20,6 +22,7 @@ srand (time(NULL));
             printf("%d\t", matriz[i][j]);
         }
         printf("\n");
+        //Mediante dos bucles for, puedo ir dando valores a mi matriz con numeros aleatorios
     }
     printf("Segunda Matriz:\n");
     for (int i = 0; i < filas; i++)
@@ -29,9 +32,11 @@ srand (time(NULL));
             printf("%d\t", matriz2[i][j]);
         }
         printf ("\n");
+        //Repito el mismo proceso
     }
     printf("Suma de matrices:\n");
     sumaMatriz(filas,columnas,matriz,matriz2,sumatriz);
+    //Aqui hago el llamado de la funcion
 
 return 0;
 }
@@ -43,6 +48,7 @@ for (int i = 0; i < filas; i++)
             printf("%d\t", sumatriz[i][j]);
         }
         printf ("\n");
+        //En la funcion, establezco que la suma de las matrices, permitiendo asi llamarla en cualquier parte del codigo
     }
 }
 
